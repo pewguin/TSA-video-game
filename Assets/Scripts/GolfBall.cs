c24743f0e4373761a8caf47d90b661cca1739a43
pewguin <jonas.brastad@gmail.com>
2025-01-02 15:47:32 -0800

    started display arrow

--- Assets/Scripts/GolfBall.cs
+++ Assets/Scripts/GolfBall.cs
@@ -1,17 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GolfBall : MonoBehaviour
 {
     Vector2 worldMousePos;
     bool playerAiming;
+    // how powerful the hit from the player is
     [SerializeField] float hitStrength = 1f;
+    // how slow ball must be going before player could hit it
     [SerializeField] float speedThreshhold = 1f;
+    // should be front, middle, back
+    [SerializeField] GameObject[] arrowSegments;
+    Vector2[] arrowSizes;
+    [SerializeField] float arrowWidth = 1f;
     Rigidbody2D rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        arrowSizes = new Vector2[arrowSegments.Length];
+        for (int i = 0; i < arrowSegments.Length; i++)
+        {
+            arrowSizes[i] = arrowSegments[i].GetComponent<SpriteRenderer>().bounds.size;
+        }
+    }
+    private void Update()
+    {
+        if (playerAiming && worldMousePos != null)
+        {
+            Vector2 ballToMouse = worldMousePos - (Vector2)transform.position;
+            Vector2 frontPosition = (Vector2)transform.position + (ballToMouse.normalized * arrowSizes[0].y/2);
+            arrowSegments[0].transform.position = frontPosition;
+            Vector3 direction = transform.position - arrowSegments[0].transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            arrowSegments[0].transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+        }
     }
     private void OnMouseDown()
     {
