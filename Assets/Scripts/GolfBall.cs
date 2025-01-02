dfcdba1cf8f266f792229ae886c956f6a7d8dfde
pewguin <jonas.brastad@gmail.com>
2025-01-02 13:58:13 -0800

    added ball control

--- Assets/Scripts/GolfBall.cs
+++ Assets/Scripts/GolfBall.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class GolfBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2 worldMousePos;
+    bool playerAiming;
+    [SerializeField] float hitStrength = 1f;
+    [SerializeField] float speedThreshhold = 1f;
+    Rigidbody2D rb;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if (rb.velocity.magnitude < speedThreshhold)
+        {
+            playerAiming = true;
+        }
+    }
+    private void OnMouseDrag()
+    {
+        if (playerAiming)
+        {
+            worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    private void OnMouseUp()
+    {
+        if (playerAiming)
+        {
+            Vector2 hitForce = (Vector2)transform.position - worldMousePos;
+            hitForce *= hitStrength;
+            rb.AddForce(hitForce);
+        }
+        playerAiming = false;
+    }
+    private void OnDrawGizmos()
+    {
+        if (playerAiming)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(worldMousePos, transform.position);
+        }
     }
 }
